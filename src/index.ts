02ddbff96d70c2cc34275f7c3bac1e3dc1ccea16
Jintao Koong <jintaokoong99@gmail.com>
2021-05-04 22:42:06 +0800

    fix: fix configurations

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,56 @@
-import { ChatClient } from 'twitch-chat-client'
-import { promises as fs } from 'fs'
+interface Config {
+  channels: string[];
+}
 
-const chatClient = ChatClient.anonymous({ channels: [''] })
+const main = async () => {
+  const { promises: fs }  = require('fs');
+  const { ChatClient } = require('twitch-chat-client');
 
-chatClient.onConnect(() => {
-  console.log('connected')
-})
+  const b = await fs.readFile('./app-config.json');
+  const config: Config = JSON.parse(b.toString());
 
-const messageListener = chatClient.onMessage((channel, user, message, msg) => {
-  // const ut = msg.userInfo.userType.length > 0 ? msg.userInfo.userType.toUpperCase() : 'U';
-  const { userType, userName } = msg.userInfo
-  const dp = msg.userInfo.displayName
-  let name = ''
-  if (dp === name) {
-    name = dp;
-  } else {
-    name = `${dp} (${userName})`;
-  }
+  const chatClient = ChatClient.anonymous({ channels: config.channels })
 
-  const millis = new Date()
-  const now = millis.toISOString()
-
-  const log = `${now} ${name}: ${message}`
-
-  fs.appendFile(
-    `./${channel.replace('#', '')}_${millis.getUTCFullYear()}${millis.getUTCMonth()}${millis.getUTCDay()}.log`,
-    `${log}\n`).then(() => {
-    console.log(log)
+  chatClient.onConnect(() => {
+    console.log('connected')
   })
-})
+
+  const messageListener = chatClient.onMessage((channel, user, message, msg) => {
+    // const ut = msg.userInfo.userType.length > 0 ? msg.userInfo.userType.toUpperCase() : 'U';
+    const { userType, userName } = msg.userInfo
+    const dp = msg.userInfo.displayName
+    let name = ''
+    if (dp === name) {
+      name = dp;
+    } else {
+      name = `${dp} (${userName})`;
+    }
+
+    const millis = new Date()
+    const now = millis.toISOString()
+
+    const log = `${now} ${name}: ${message}`
+
+    fs.appendFile(
+      `./${channel.replace('#', '')}_${millis.getUTCFullYear()}${millis.getUTCMonth()}${millis.getUTCDay()}.log`,
+      `${log}\n`).then(() => {
+      console.log(log)
+    })
+  })
 
 
-const disconnectListener = chatClient.onDisconnect(((manually, reason) => {
-  console.log(manually, reason)
-}))
+  const disconnectListener = chatClient.onDisconnect(((manually, reason) => {
+    console.log(manually, reason)
+  }))
 
-process.on('SIGNINT', () => {
-  chatClient.removeListener(messageListener)
-  chatClient.removeListener(disconnectListener)
-});
+  process.on('SIGNINT', () => {
+    chatClient.removeListener(messageListener)
+    chatClient.removeListener(disconnectListener)
+  });
+
+  await chatClient.connect()
+}
 
 (async () => {
-  await chatClient.connect()
+  await main();
 })()
