995b949a5c348adaf5fa28148b66c719a741c901
Jintao Koong <jintaokoong99@gmail.com>
2021-05-04 22:05:47 +0800

    feat: log formatting
    - update log formatting

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,45 @@
 import { ChatClient } from 'twitch-chat-client'
-import { promises as fs } from 'fs';
+import { promises as fs } from 'fs'
 
-const chatClient = ChatClient.anonymous({ channels: [''] });
+const chatClient = ChatClient.anonymous({ channels: [''] })
 
 chatClient.onConnect(() => {
-    console.log('connected');
+  console.log('connected')
 })
 
-const messageListener = chatClient.onMessage((_channel, user, message, msg) => {
-    // const ut = msg.userInfo.userType.length > 0 ? msg.userInfo.userType.toUpperCase() : 'U';
-    const dp = msg.userInfo.displayName;
-    const millis = new Date();
-    const now = millis.toISOString();
-    const log = `${now} ${dp}: ${message}`;
-    fs.appendFile(`./${millis.getUTCFullYear()}${millis.getUTCMonth()}${millis.getUTCDay()}.log`, `${log}\n`).then(() => {
-        console.log(log);
-    });
-});
+const messageListener = chatClient.onMessage((channel, user, message, msg) => {
+  // const ut = msg.userInfo.userType.length > 0 ? msg.userInfo.userType.toUpperCase() : 'U';
+  const { userType, userName } = msg.userInfo
+  const dp = msg.userInfo.displayName
+  let name = ''
+  if (dp === name) {
+    name = dp;
+  } else {
+    name = `${dp} (${userName})`;
+  }
+
+  const millis = new Date()
+  const now = millis.toISOString()
+
+  const log = `${now} ${name}: ${message}`
+
+  fs.appendFile(
+    `./${channel.replace('#', '')}_${millis.getUTCFullYear()}${millis.getUTCMonth()}${millis.getUTCDay()}.log`,
+    `${log}\n`).then(() => {
+    console.log(log)
+  })
+})
+
 
 const disconnectListener = chatClient.onDisconnect(((manually, reason) => {
-    console.log(manually, reason);
+  console.log(manually, reason)
 }))
 
 process.on('SIGNINT', () => {
-    chatClient.removeListener(messageListener);
-    chatClient.removeListener(disconnectListener);
+  chatClient.removeListener(messageListener)
+  chatClient.removeListener(disconnectListener)
 });
 
-chatClient.connect().then(() => {
-
-});
-
+(async () => {
+  await chatClient.connect()
+})()
